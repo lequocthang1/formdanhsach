98c607c3fdc42b6ffbfbc32c27f54e6c95e1b250
IDS\ha.vo <ha.vo@idsolutions.com.vn>
2024-12-09 12:01:46 +0700

    preview code

--- java.js
+++ java.js
@@ -1,184 +1,161 @@
 function toggleButton(button) {
-    button.classList.toggle("active");
+  button.classList.toggle("active");
 }
 function updateButtonText(action, color) {
-    const button = document.getElementById("buttonText");
-    button.textContent = action;
-    button.style.backgroundColor = color;
-}  
+  const button = document.getElementById("buttonText");
+  button.textContent = action;
+  button.style.backgroundColor = color;
+}
 function toggleDropdown() {
-    const dropdownContent = document.querySelector(".dropdown-content");
-    dropdownContent.style.display =
+  const dropdownContent = document.querySelector(".dropdown-content");
+  dropdownContent.style.display =
     dropdownContent.style.display === "block" ? "none" : "block";
 }
 
 // -----------------------------//
 
 function toggleButton1(button) {
-    button.classList.toggle("active");
+  button.classList.toggle("active");
 }
-    function updateButtonText1(action, color) {
-    const button = document.getElementById("buttonText1");
-    button.textContent = action;
-    button.style.backgroundColor = color;
-}  
+function updateButtonText1(action, color) {
+  const button = document.getElementById("buttonText1");
+  button.textContent = action;
+  button.style.backgroundColor = color;
+}
 function toggleDropdown1() {
-    const dropdownContent = document.querySelector("#dropdown-content");
-    dropdownContent.style.display =
+  const dropdownContent = document.querySelector("#dropdown-content");
+  dropdownContent.style.display =
     dropdownContent.style.display === "block" ? "none" : "block";
 }
 
 // -----------------------------//
 
 function toggleButton2(button) {
-    button.classList.toggle("active");
+  button.classList.toggle("active");
 }
 function updateButtonText2(action, color) {
-    const button = document.getElementById("buttonText2");
-    button.textContent = action;
-    button.style.backgroundColor = color;
-}  
+  const button = document.getElementById("buttonText2");
+  button.textContent = action;
+  button.style.backgroundColor = color;
+}
 function toggleDropdown2() {
-    const dropdownContent = document.querySelector("#dropdown-content2");
-    dropdownContent.style.display =
+  const dropdownContent = document.querySelector("#dropdown-content2");
+  dropdownContent.style.display =
     dropdownContent.style.display === "block" ? "none" : "block";
 }
 
- // -----------------------------//
+// -----------------------------//
 
 function toggleButton3(button) {
-    button.classList.toggle("active");
+  button.classList.toggle("active");
 }
-    function updateButtonText3(action, color) {
-    const button = document.getElementById("buttonText3");
-    button.textContent = action;
-    button.style.backgroundColor = color;
-}  
+function updateButtonText3(action, color) {
+  const button = document.getElementById("buttonText3");
+  button.textContent = action;
+  button.style.backgroundColor = color;
+}
 function toggleDropdown3() {
-    const dropdownContent = document.querySelector("#dropdown-content3");
-    dropdownContent.style.display =
+  const dropdownContent = document.querySelector("#dropdown-content3");
+  dropdownContent.style.display =
     dropdownContent.style.display === "block" ? "none" : "block";
 }
 
- // -----------------------------//
+// -----------------------------//
 
 function toggleButton4(button) {
-    button.classList.toggle("active");
+  button.classList.toggle("active");
 }
-    function updateButtonText4(action, color) {
-    const button = document.getElementById("buttonText4");
-    button.textContent = action;
-    button.style.backgroundColor = color;
-}  
+function updateButtonText4(action, color) {
+  const button = document.getElementById("buttonText4");
+  button.textContent = action;
+  button.style.backgroundColor = color;
+}
 function toggleDropdown4() {
-    const dropdownContent = document.querySelector("#dropdown-content4");
-    dropdownContent.style.display =
+  const dropdownContent = document.querySelector("#dropdown-content4");
+  dropdownContent.style.display =
     dropdownContent.style.display === "block" ? "none" : "block";
 }
 
 // -----------------------------//
 
 function toggleButton5(button) {
-    button.classList.toggle("active");
+  button.classList.toggle("active");
 }
-    function updateButtonText5(action, color) {
-    const button = document.getElementById("buttonText5");
-    button.textContent = action;
-    button.style.backgroundColor = color;
-}  
+function updateButtonText5(action, color) {
+  const button = document.getElementById("buttonText5");
+  button.textContent = action;
+  button.style.backgroundColor = color;
+}
 function toggleDropdown5() {
-    const dropdownContent = document.querySelector("#dropdown-content5");
-    dropdownContent.style.display =
+  const dropdownContent = document.querySelector("#dropdown-content5");
+  dropdownContent.style.display =
     dropdownContent.style.display === "block" ? "none" : "block";
 }
 
 // -----------------------------//
 
 function toggleButton6(button) {
-    button.classList.toggle("active");
+  button.classList.toggle("active");
 }
 function updateButtonText6(action, color) {
-    const button = document.getElementById("buttonText6");
-    button.textContent = action;
-    button.style.backgroundColor = color;
-}  
+  const button = document.getElementById("buttonText6");
+  button.textContent = action;
+  button.style.backgroundColor = color;
+}
 function toggleDropdown6() {
-    const dropdownContent = document.querySelector("#dropdown-content6");
-    dropdownContent.style.display =
+  const dropdownContent = document.querySelector("#dropdown-content6");
+  dropdownContent.style.display =
     dropdownContent.style.display === "block" ? "none" : "block";
 }
 
 // -----------------------------//
 
 function toggleButton7(button) {
-    button.classList.toggle("active");
+  button.classList.toggle("active");
 }
-    function updateButtonText7(action, color) {
-    const button = document.getElementById("buttonText7");
-    button.textContent = action;
-    button.style.backgroundColor = color;
-}  
+function updateButtonText7(action, color) {
+  const button = document.getElementById("buttonText7");
+  button.textContent = action;
+  button.style.backgroundColor = color;
+}
 function toggleDropdown7() {
-    const dropdownContent = document.querySelector("#dropdown-content7");
-    dropdownContent.style.display =
+  const dropdownContent = document.querySelector("#dropdown-content7");
+  dropdownContent.style.display =
     dropdownContent.style.display === "block" ? "none" : "block";
 }
 
 // -----------------------------//
+// FIXME: Tách hàm để rõ nghĩa cho người đọc và dễ bảo trì
 
-const checkAll = document.getElementById('check-all');
-const checkItems = document.querySelectorAll('.check-item');
+const checkAll = document.getElementById("check-all");
+const checkItems = document.querySelectorAll(".check-item");
 
-    checkAll.addEventListener('change', function() {
-    checkItems.forEach(item => {
-        item.checked = checkAll.checked; 
-    });
-});
-
-    checkItems.forEach(item => {
-        item.addEventListener('change', function() { 
-    checkAll.checked = [...checkItems].every(item => item.checked);
-    });
-});
-
-// -----------------------------//
-
-document
-  .getElementById("toggleButton")
-  .addEventListener("click", function () {
-    const formElements = document.getElementById("formElements");
-    if (formElements.classList.contains("hidden")) {
-      formElements.classList.remove("hidden");
-    } else {
-      formElements.classList.add("hidden");   
-    }
+checkAll.addEventListener("change", function () {
+  checkItems.forEach((item) => {
+    item.checked = checkAll.checked;
   });
+});
 
-  // -----------------------------//
-
-  document.querySelector(".loc").addEventListener("click", function (e) {
-    const button = e.currentTarget;
-    const rect = button.getBoundingClientRect();
-
-    const ripple = document.createElement("span");
-    ripple.classList.add("ripple");
-
-    const rippleX = e.clientX - rect.left;
-    const rippleY = e.clientY - rect.top;
-
-    ripple.style.left = `${rippleX - 50}px`;
-    ripple.style.top = `${rippleY - 50}px`;
-
-    button.appendChild(ripple);
-
-    ripple.addEventListener("animationend", () => {
-    ripple.remove();
+checkItems.forEach((item) => {
+  item.addEventListener("change", function () {
+    checkAll.checked = [...checkItems].every((item) => item.checked);
   });
 });
 
 // -----------------------------//
 
-document.querySelector(".dongbosanpham").addEventListener("click", function (e) {
+document.getElementById("toggleButton").addEventListener("click", function () {
+  const formElements = document.getElementById("formElements");
+  if (formElements.classList.contains("hidden")) {
+    formElements.classList.remove("hidden");
+  } else {
+    formElements.classList.add("hidden");
+  }
+});
+
+// -----------------------------//
+
+document.querySelector(".loc").addEventListener("click", function (e) {
   const button = e.currentTarget;
   const rect = button.getBoundingClientRect();
 
@@ -197,6 +174,30 @@ document.querySelector(".dongbosanpham").addEventListener("click", function (e) 
     ripple.remove();
   });
 });
+
+// -----------------------------//
+
+document
+  .querySelector(".dongbosanpham")
+  .addEventListener("click", function (e) {
+    const button = e.currentTarget;
+    const rect = button.getBoundingClientRect();
+
+    const ripple = document.createElement("span");
+    ripple.classList.add("ripple");
+
+    const rippleX = e.clientX - rect.left;
+    const rippleY = e.clientY - rect.top;
+
+    ripple.style.left = `${rippleX - 50}px`;
+    ripple.style.top = `${rippleY - 50}px`;
+
+    button.appendChild(ripple);
+
+    ripple.addEventListener("animationend", () => {
+      ripple.remove();
+    });
+  });
 
 // -----------------------------//
 
@@ -224,29 +225,33 @@ document.querySelector(".add-sanpham").addEventListener("click", function (e) {
 
 const input = document.getElementById("finditem");
 
-      input.addEventListener("focus", () => {
-        input.style.width = "500px"; // Tăng kích thước khi focus
-      });
+input.addEventListener("focus", () => {
+  input.style.width = "500px"; // Tăng kích thước khi focus
+});
 
-      input.addEventListener("blur", () => {
-        input.style.width = "20%"; // Trở lại kích thước ban đầu khi blur
-      });
+input.addEventListener("blur", () => {
+  input.style.width = "20%"; // Trở lại kích thước ban đầu khi blur
+});
 
 // -----------------------------//
 
-const apiURL = 'https://6752728bd1983b9597b6399b.mockapi.io/api/danhsach';
+const apiURL = "https://6752728bd1983b9597b6399b.mockapi.io/api/danhsach";
 
 // Lấy bảng từ DOM
-const tableBody = document.querySelector('#data-table tbody');
+const tableBody = document.querySelector("#data-table tbody");
 
 // Lấy dữ liệu từ MockAPI
+// FIXME: Tách hàm để rõ nghĩa cho người đọc và dễ bảo trì
 fetch(apiURL)
-  .then(response => response.json())  // Chuyển đổi dữ liệu JSON
-  .then(data => {
+  .then((response) => response.json()) // Chuyển đổi dữ liệu JSON
+  .then((data) => {
     // Lặp qua mỗi mục dữ liệu và chèn vào bảng
-    data.forEach(user => {
-      const row = document.createElement('tr');
-      
+    data.forEach((user) => {
+      const row = document.createElement("tr");
+
+      //   FIXME: Tên field trong model phải đặt theo một quy chuẩn
+      // FIXME: Tên hàm củng phải đặt theo một quy chuẩn (dòng 272, 272, ...)
+      // TODO: Tham khảo link: https://viblo.asia/p/javascript-naming-conventions-1Je5Ea1A5nL
       row.innerHTML = `
         <td><input type="checkbox" class="checkbox" data-id="${user.masanpham}"></td>
         <th>${user.masanpham}</th>
@@ -271,9 +276,9 @@ fetch(apiURL)
             <i class="fas fa-trash-alt" id="toggleXoa" onclick="toggleXoa()"></i>
         </th>
         `;
-      
+
       // Thêm hàng vào bảng
       tableBody.appendChild(row);
     });
   })
-  .catch(error => console.log('Lỗi khi lấy dữ liệu:', error));
+  .catch((error) => console.log("Lỗi khi lấy dữ liệu:", error));
